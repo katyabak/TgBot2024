634a7e14b36b7f42a5d4e794c60d7ce1439ed573
Ekaterina Baklanova <124804706+katyabak@users.noreply.github.com>
2024-04-23 16:15:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from handlers import bot
 from settingsHandle import handle_settings, select_favorite_category
 from weatherHandle import handle_weather
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
